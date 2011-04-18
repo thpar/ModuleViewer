9015931de3fbdb378c8c4bc52c4d8b16f1413e1a
thpar <thpar@psb.ugent.be>
2011-04-18 10:19:06 +0000

    Adding parsers

---
+++ be/ugent/psb/moduleviewer/parsers/DataMatrixParser.java
@@ -0,0 +1,20 @@
+package be.ugent.psb.moduleviewer.parsers;
+
+
+/**
+ * Parses the initial expression matrix.
+ * 
+ * Format:
+ * 
+ * line 1 : tab delimited list of conditions (ids)
+ * other lines: tab delimited list: 
+ * column 0: gene id
+ * column 1: optional description
+ * column 2 and further: values in same order as conditions
+ * 
+ * @author thpar
+ *
+ */
+public class DataMatrixParser extends Parser {
+
+}
---
+++ be/ugent/psb/moduleviewer/parsers/MVFParser.java
@@ -0,0 +1,38 @@
+package be.ugent.psb.moduleviewer.parsers;
+
+
+/**
+ * Parses files of the .mvf type, the native ModuleViewer Format.
+ * 
+ * Format:
+ * 
+ * Start block:
+ * List of key value pairs. One pair per line. A key value line starts with "::". 
+ * Key and value are separated by "=".
+ * Keys are in capitals.
+ * 
+ * Known key examples:
+ * ::TYPE=type
+ * ::COLOR=red
+ * ::SEP=:
+ * 
+ * TYPE defines the type of data we're reading. Can be used as a label for the 
+ * matrix that is drawn.
+ * 
+ * COLOR is an optional setting to tell the viewer which color to use when displaying this data.
+ * 
+ * SEP optional separator value. When set, each gene entry from a list will be considered to exist of GeneID<SEP>value
+ * 
+ * 
+ * The rest of the lines of a block are tab delimited:
+ * Col 0 : ModuleId
+ * Col 1 : Gene list, pipe delimited.
+ * Col 2 : Optional. Describes which set this list of genes belongs to (a GO term, etc...) When empty, the TYPE value will be
+ * used.
+ * 
+ * @author thpar
+ *
+ */
+public class MVFParser extends Parser {
+
+}
