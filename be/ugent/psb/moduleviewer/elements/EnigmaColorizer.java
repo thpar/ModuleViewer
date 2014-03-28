372c33a2690c4e237c4de4057bda05b169c2ee37
thpar <thpar@psb.ugent.be>
2014-03-28 11:25:44 +0100

    Allow the EnigmaColorizer to flip color scale

--- be/ugent/psb/moduleviewer/elements/EnigmaColorizer.java
+++ be/ugent/psb/moduleviewer/elements/EnigmaColorizer.java
@@ -8,12 +8,45 @@ public class EnigmaColorizer extends ExpressionColorizer{
 	private double sigma;
 	private double mean;
 	
+	private Scale scale = Scale.BLUE_TO_YELLOW;
 	
+	/**
+	 * For low expression to high, make the scale blue to yellow or the
+	 * other way around?
+	 * 
+	 * @author Thomas Van Parys
+	 *
+	 */
+	enum Scale{
+		YELLOW_TO_BLUE, BLUE_TO_YELLOW;
+	}
+	
+	/**
+	 * Initialize an {@link ExpressionColorizer} using given sigma and mean. The scale determines 
+	 * whether expression levels from low to high are drawn as blue to yellow or the other way around.
+	 * 
+	 * @param sigma
+	 * @param mean
+	 * @param scale
+	 */
+	public EnigmaColorizer(double sigma, double mean, Scale scale) {
+		this.sigma = sigma;
+		this.mean = mean;
+		this.scale = scale;
+	}
+	
+	/**
+	 * Initialize an {@link ExpressionColorizer} using given sigma and mean.
+	 * Expression levels are drawn by default as blue (low) to yellow (high)
+	 * 
+	 * @param sigma
+	 * @param mean
+	 */
 	public EnigmaColorizer(double sigma, double mean) {
 		this.sigma = sigma;
 		this.mean = mean;
 	}
-
+	
 
 	@Override
 	public Color getColor(Double data) {
@@ -21,9 +54,22 @@ public class EnigmaColorizer extends ExpressionColorizer{
 		
 		Color col;
 		float blueperc, yellowperc;
-		blueperc = new Float(upProb(data,mean,sigma));
-		yellowperc = new Float(downProb(data,mean,sigma));
-
+		float up, down;
+		up = new Float(upProb(data,mean,sigma));
+		down= new Float(downProb(data,mean,sigma));
+		
+		switch(scale){
+		default:
+		case BLUE_TO_YELLOW:
+			blueperc = down;
+			yellowperc = up;
+			break;
+		case YELLOW_TO_BLUE:
+			blueperc = up;
+			yellowperc = down;
+			break;
+		}
+		
 		col = new Color(yellowperc, yellowperc, blueperc);
 
 		return col;
