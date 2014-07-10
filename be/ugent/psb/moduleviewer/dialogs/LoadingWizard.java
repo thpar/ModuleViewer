1863305830e09baaa583539515f1b98b5f0ee930
thpar <thpar@psb.ugent.be>
2014-07-10 14:46:48 +0200

    Functional data loading wizard dialog.

--- be/ugent/psb/moduleviewer/dialogs/LoadingWizard.java
+++ be/ugent/psb/moduleviewer/dialogs/LoadingWizard.java
@@ -18,16 +18,29 @@ import javax.swing.JPanel;
 import be.ugent.psb.moduleviewer.actions.FileNameRegexFilter;
 import be.ugent.psb.moduleviewer.model.GUIModel;
 
+/**
+ * This modal dialogue presents the user with the list of required and optional loading options.
+ * 
+ * 
+ * @author Thomas Van Parys
+ *
+ */
 public class LoadingWizard extends JDialog implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	
+	//button actions
 	private final static String NEXT = "next"; 
 	private final static String BACK = "back"; 
-	
 	private final static String CANCEL = "cancel"; 
 	private final static String FINISH = "finish"; 
 
+	//exit states
+	public final static int CANCELLED = 0;
+	public final static int APPROVED = 1;
+	
+	
+	private int returnValue = CANCELLED;
 	
 	List<WizardPage> pages = new ArrayList<WizardPage>();
 
@@ -43,6 +56,11 @@ public class LoadingWizard extends JDialog implements ActionListener{
 
 	private JButton backButton;
 	
+	/**
+	 * Constructu the wizard dialog. 
+	 * 
+	 * @param guiModel the GUIModel.
+	 */
 	public LoadingWizard(GUIModel guiModel){
 		this.guiModel = guiModel;
 		
@@ -53,31 +71,35 @@ public class LoadingWizard extends JDialog implements ActionListener{
 		createPages();
 		
 		this.setTitle(pages.get(currentPageNumber).getTitle());
-		mainPanel.add(pages.get(currentPageNumber), BorderLayout.CENTER);
+		mainPanel.add(pages.get(currentPageNumber),BorderLayout.NORTH);
 		mainPanel.add(createButtonPanel(), BorderLayout.SOUTH);
 		
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
+		this.setModal(true);
 		this.validate();
 		this.pack();
 
 	}
 	
+	/**
+	 * Generates the separate wizard pages and add them to a list.
+	 */
 	private void createPages() {
-		pages.add(new WizardPage(guiModel, "Load expression data", 
+		pages.add(new WizardPage(this, "Load expression data", 
 				"<html>Select the file with expression data. "
 				+ "This tab delimited matrix should have all conditions on the first line. Each subsquent line contains the gene id and its "
 				+ "expression values.</html>"));
-		pages.add(new WizardPage(guiModel, "Load gene clusters", 
+		pages.add(new WizardPage(this, "Load gene clusters", 
 				"<html>The clustering of genes into modules. Each line has a module number and a pipe delimited list of gene ids.</html>"));
-		pages.add(new WizardPage(guiModel, "Load gene id mapping",
+		pages.add(new WizardPage(this, "Load gene id mapping",
 				"<html>A two column file with gene ids in the first and symbolic names in the second column.</html>", true));
-		pages.add(new WizardPage(guiModel, "Load condition clusters",
+		pages.add(new WizardPage(this, "Load condition clusters",
 				"<html>Subclustering of conditions with similar expression patterns.</html>", new FileNameRegexFilter("XML files", ".*\\.xml"), true));
-		pages.add(new WizardPage(guiModel, "Load regulator clusters", 
+		pages.add(new WizardPage(this, "Load regulator clusters", 
 				"<html>The list of regulators for each module. Each line needs a module id "
 				+ "and a pipe delimited list of regulator gene ids.</html>", true));
-		pages.add(new WizardPage(guiModel, "Load annotations", 
+		pages.add(new WizardPage(this, "Load annotations", 
 				"<html>All integrated data that needs to be mapped onto the expression data in .mvf format. "
 				+ "the .mvf file can contain multiple annotation blocks. Each block should"
 				+ "start with a header defining its behaviour. The content of a block "
@@ -85,9 +107,15 @@ public class LoadingWizard extends JDialog implements ActionListener{
 				new FileNameRegexFilter("MVF files", ".*\\.mvf"), true));
 	}
 
+	/**
+	 * Create the panel containing the navigation and submit buttons.
+	 * 
+	 * @return the panel containing the navigation and submit buttons.
+	 */
 	private JPanel createButtonPanel(){
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+		buttonPanel.setAlignmentX(LEFT_ALIGNMENT);
 		buttonPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		
 		JButton cancelButton = new JButton("Cancel");
@@ -119,7 +147,10 @@ public class LoadingWizard extends JDialog implements ActionListener{
 	}
 	
 	
-
+	/**
+	 * 
+	 * @return true when the required files have been selected. 
+	 */
 	private boolean ready() {
 		for (WizardPage page : pages){
 			if (!page.isOptional() && page.getFile()==null){
@@ -139,31 +170,38 @@ public class LoadingWizard extends JDialog implements ActionListener{
 		} else if (action.equals(CANCEL)){
 			this.dispose();
 		} else if (action.equals(FINISH)){
-			this.startLoading();
+			this.returnValue = APPROVED;
 			this.dispose();
 		}
 	}
 	
-	private void startLoading() {
-		for (WizardPage page : pages){
-			File file = page.getFile();
-			System.out.println(file);
-		}
-		
-	}
 
 	@Override
 	public void setTitle(String title){
 		super.setTitle("Load data wizard - "+title);
 	}
 
+	/**
+	 * Go to the previous page.
+	 */
 	private void previousPage() {
 		switchPage(currentPageNumber-1);		
 	}
+	
+	/**
+	 * Go to the next page.
+	 */
 	private void nextPage() {
 		switchPage(currentPageNumber+1);		
 	}
 
+	/**
+	 * Go to the given page. The old page will be removed from the layout
+	 * and replaced with the new page. The state of the navigation buttons will
+	 * be refreshed and the dialog will be revalidated and repainted.
+	 * 
+	 * @param newPage number of the page to switch to.
+	 */
 	private void switchPage(int newPage) {
 		int oldPageNumber = currentPageNumber;
 		currentPageNumber = newPage;
@@ -177,16 +215,53 @@ public class LoadingWizard extends JDialog implements ActionListener{
 			WizardPage currentPage = pages.get(currentPageNumber);
 			this.setTitle(currentPage.getTitle());
 			this.mainPanel.remove(oldPage);
-			this.mainPanel.add(currentPage, BorderLayout.CENTER);
+			this.mainPanel.add(currentPage, BorderLayout.NORTH);
 			
-			nextButton.setEnabled(this.currentPageNumber<pages.size()-1);
-			backButton.setEnabled(this.currentPageNumber>0);
-			finishButton.setEnabled(this.ready());
+			this.refreshButtons();
 			
 			this.revalidate();
 			this.repaint();
 		}
 			
+	}
+	
+	/**
+	 * 
+	 * @return the GUIModel.
+	 */
+	public GUIModel getGuiModel() {
+		return guiModel;
+	}
+
+	/**
+	 * Set the state of the navigation buttons depending on the currentpage
+	 * and the files that have already been loaded.
+	 */
+	public void refreshButtons(){
+		nextButton.setEnabled(this.currentPageNumber<pages.size()-1);
+		backButton.setEnabled(this.currentPageNumber>0);
+		finishButton.setEnabled(this.ready());		
+	}
+	
+	/**
+	 * Return the exit state of the dialog. Constants can be CANCELLED or APPROVED.
+	 * @return the exit state of the dialog.
+	 */
+	public int getReturnValue(){
+		return returnValue;
+	}
+	
+	/**
+	 * a list of files, selected throught the wizard.
+	 * 
+	 * @return a list of files, selected throught the wizard.
+	 */
+	public List<File> getSelectedFiles(){
+		List<File> selectedFiles = new ArrayList<File>();
+		for (WizardPage page: pages){
+			selectedFiles.add(page.getFile());
+		}
+		return selectedFiles;
 	}
 
 }
--- be/ugent/psb/moduleviewer/dialogs/WizardPage.java
+++ be/ugent/psb/moduleviewer/dialogs/WizardPage.java
@@ -1,10 +1,12 @@
 package be.ugent.psb.moduleviewer.dialogs;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -15,6 +17,12 @@ import javax.swing.JTextField;
 import be.ugent.psb.moduleviewer.actions.FileNameRegexFilter;
 import be.ugent.psb.moduleviewer.model.GUIModel;
 
+/**
+ * A page from the data {@link LoadingWizard}.
+ * 
+ * @author Thomas Van Parys
+ *
+ */
 public class WizardPage extends JPanel implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
@@ -23,47 +31,104 @@ public class WizardPage extends JPanel implements ActionListener{
 	private GUIModel guiModel;
 	private FileNameRegexFilter filter;
 	private boolean optional = false;
+	private LoadingWizard wizard;
 	
-	public WizardPage(GUIModel guiModel, String title, String descr){
-		this(guiModel, title, descr, null, false);
+	/**
+	 * Create a new wizard page.
+	 * 
+	 * @param wizard the parent dialog {@link LoadingWizard}
+	 * @param title title for this page, the content and the {@link JFileChooser}
+	 * @param descr description of the file content.
+	 */
+	public WizardPage(LoadingWizard wizard, String title, String descr){
+		this(wizard, title, descr, null, false);
 	}
-	public WizardPage(GUIModel guiModel, String title, String descr, boolean optional){
-		this(guiModel, title, descr, null, optional);
+	
+	/**
+	 * Create a new wizard page.
+	 * 
+	 * @param wizard the parent dialog {@link LoadingWizard}
+	 * @param title title for this page, the content and the {@link JFileChooser}
+	 * @param descr description of the file content.
+	 * @param optional states if the file loaded on this page is optional (default: false)
+	 */
+	public WizardPage(LoadingWizard wizard, String title, String descr, boolean optional){
+		this(wizard, title, descr, null, optional);
 	}
-	public WizardPage(GUIModel guiModel, String title, String descr, FileNameRegexFilter extensions, boolean optional){
-		this.guiModel = guiModel;
+	
+	/**
+	 * Create a new wizard page.
+	 * 
+	 * @param wizard the parent dialog {@link LoadingWizard}
+	 * @param title title for this page, the content and the {@link JFileChooser}
+	 * @param descr description of the file content.
+	 * @param extensions regular expression and description of the file extensions.
+	 * @param optional states if the file loaded on this page is optional (default: false)
+	 */
+	public WizardPage(LoadingWizard wizard, String title, String descr, FileNameRegexFilter extensions, boolean optional){
+		this.wizard = wizard;
+		this.guiModel = wizard.getGuiModel();
 		this.title = title;
 		this.filter = extensions;
 		this.optional = optional;
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.setBorder(BorderFactory.createLoweredBevelBorder());
+		this.setAlignmentX(LEFT_ALIGNMENT);
+		this.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLoweredBevelBorder(),
+				BorderFactory.createEmptyBorder(10, 10, 10, 10) 
+			));
 		
-		this.add(new JLabel(descr));
-		if (optional){
-			this.add(new JLabel("(Optional)"));			
-		}
+		JPanel descrPanel = new JPanel();
+		descrPanel.setAlignmentX(LEFT_ALIGNMENT);
+		descrPanel.setLayout(new BoxLayout(descrPanel, BoxLayout.PAGE_AXIS));
+		String titleLabelText = optional? title+" (optional)":title;
+		descrPanel.add(new JLabel(titleLabelText));
+		descrPanel.add(Box.createVerticalStrut(5));
+		descrPanel.add(new JLabel(descr));
+		descrPanel.setPreferredSize(new Dimension(400,200));
+		this.add(descrPanel);
 		
-		fileTextField = new JTextField();
-		this.add(fileTextField);
+		JPanel browsePanel = new JPanel();
+		browsePanel.setLayout(new BoxLayout(browsePanel,BoxLayout.LINE_AXIS));
+		browsePanel.setAlignmentX(LEFT_ALIGNMENT);
+		fileTextField = new JTextField(40);
+		browsePanel.add(fileTextField);
 		JButton browseButton = new JButton("Browse...");
 		browseButton.addActionListener(this);
-		this.add(browseButton);
+		browsePanel.add(browseButton);
+		this.add(browsePanel);
 		
 	}
 	
 	
-
+	/**
+	 * 
+	 * @return true if the file loaded on this page is optional
+	 */
 	public boolean isOptional() {
 		return optional;
 	}
+	
+	/**
+	 * 
+	 * @param optional true if the file loaded on this page is optional
+	 */
 	public void setOptional(boolean optional) {
 		this.optional = optional;
 	}
+	
+	/**
+	 * 
+	 * @return the title of this page
+	 */
 	public String getTitle() {
 		return this.title;
 	}
 
-	
+	/**
+	 * 
+	 * @return the file selected on this page. Returns null if the file doesn't exist.
+	 */
 	public File getFile() {
 		File file = new File(this.fileTextField.getText());
 		if (file.exists()){
@@ -84,7 +149,7 @@ public class WizardPage extends JPanel implements ActionListener{
 		if (answer == JFileChooser.APPROVE_OPTION){
 			this.fileTextField.setText(fc.getSelectedFile().getAbsolutePath());
 		}
-		
+		wizard.refreshButtons();
 	}
 	
 	
